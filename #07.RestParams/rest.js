bc17e98efea12a742ff6b4354a489868c5e1f92e
MerrilBds <Bdsmerril@gmail.com>
2023-07-06 14:59:38 +0200

    leçon 7 fin

---
+++ #07.RestParams/rest.js
@@ -0,0 +1,16 @@
+/*
+Use the Rest Parameter with Function Parameters
+In order to help us create more flexible functions, 
+ES6 introduces the rest parameter for function parameters.
+ With the rest parameter, you can create functions 
+ that take a variable number of arguments. 
+These arguments are stored in an array 
+that can be accessed later from inside the function.
+*/
+const sum = (...args) => {
+    let total = 0;
+    for (let i = 0; i < args.length; i++) {
+        total += args[i];
+    }
+    return total;
+}
