304eaa9d832e7f47a4d34b1607f0c3b872a0cd28
MerrilBds <Bdsmerril@gmail.com>
2023-07-07 11:53:29 +0200

    leçon 13 fin

---
+++ #13.DestructAnArray(with_rest)/des.js
@@ -0,0 +1,24 @@
+/*
+Destructuring via rest elements
+
+In some situations involving array destructuring, we might want to collect the rest of the elements into a separate array.
+
+The result is similar to Array.prototype.slice(), as shown below:
+
+const [a, b, ...arr] = [1, 2, 3, 4, 5, 7];
+console.log(a, b);
+console.log(arr);
+
+The console would display the values 1, 2 and [3, 4, 5, 7].
+*/
+// similar to Array.prototype.slice()
+
+function removeFirstTwo(list) {
+
+    const [, , ...shorterList] = list;
+
+    return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
