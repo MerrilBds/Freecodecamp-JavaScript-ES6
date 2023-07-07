8c4c9239215fc6ee1940eb9b0dcb51162fb8a0da
MerrilBds <Bdsmerril@gmail.com>
2023-07-07 14:20:36 +0200

    leçon 21 fin

--- #22.Import-a-Function/impo.js
+++ #22.Import-a-Function/impo.js
@@ -1,1 +1,4 @@
-//Reuse JavaScript Code Using import
+import { uppercaseString, lowercaseString } from '../#21.Export-a-Function/string_functions.js';
+
+console.log(uppercaseString("hello"));
+console.log(lowercaseString("WORLD!"));
